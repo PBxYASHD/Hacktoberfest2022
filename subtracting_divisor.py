76432d1b027d47c4b4c1744e71e0e4ede6467fbb
Yash Kumar Dhurve <88287637+PBxYASHD@users.noreply.github.com>
2022-10-02 12:53:40 +0530

    subtracting_divisor

---
+++ subtracting_divisor.py
@@ -0,0 +1,23 @@
+
+
+class Solution:
+    def numsGame(self, N):
+        # code here
+        if N%2==0:
+            return 1
+        else:
+            return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.numsGame(N))
+# } Driver Code Ends
